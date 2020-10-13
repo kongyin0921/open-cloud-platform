7193dfa1e776d114caae67632ee799ab63204687
kongyin <1401618234@qq.com>
2020-10-13 21:51:17 +0800

    服务注册发现

---
+++ register-center/eureka-server/src/main/java/com/ycz/platform/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.ycz.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
