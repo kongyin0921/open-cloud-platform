49151736f47d79daedaa60ca580ac512184ecff4
kongyin <1401618234@qq.com>
2020-10-14 22:58:56 +0800

    非关系型数据存储

---
+++ inner-intergration/redis-spring-boot-starter/src/main/java/com/open/capacity/redis/RedisAutoConfig.java
@@ -0,0 +1,45 @@
+package com.open.capacity.redis;
+
+import io.lettuce.core.RedisClient;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * redis自动配置
+ * @author kong
+ * @date 2020-10-14 21:20
+ */
+@Configuration
+@EnableCaching
+@SuppressWarnings("all")
+@AutoConfigureBefore(RedisTemplate.class)
+@EnableConfigurationProperties(RedissonProperties.class)
+public class RedisAutoConfig {
+
+    @Autowired
+    private RedissonProperties redissonProperties;
+
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean(destroyMethod = "destroy")
+    @ConditionalOnClass(RedisClient.class)
+    public LettuceConnectionFactory lettuceConnectionFactory(GenericObjectPoolConfig genericObjectPoolConfig){
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
+    }
+
+}
---
+++ inner-intergration/redis-spring-boot-starter/src/main/java/com/open/capacity/redis/RedissonProperties.java
@@ -0,0 +1,16 @@
+package com.open.capacity.redis;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 配置属性
+ * @author kong
+ * @date 2020-10-14 21:32
+ */
+@Data
+@ConfigurationProperties(prefix = "spring.redis.redisson")
+public class RedissonProperties {
+    private String config;
+    private String enable;
+}
