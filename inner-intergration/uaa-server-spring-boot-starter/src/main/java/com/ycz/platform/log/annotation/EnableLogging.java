30540cd897e5d3628e36d825c922067be1d25e3c
kongyin <1401618234@qq.com>
2020-10-21 17:21:26 +0800

    日志

---
+++ inner-intergration/uaa-server-spring-boot-starter/src/main/java/com/ycz/platform/log/annotation/EnableLogging.java
@@ -0,0 +1,13 @@
+package com.ycz.platform.log.annotation;
+
+import java.lang.annotation.Target;
+
+/**
+ * @ClassName EnableLogging
+ * @Description 开启日志
+ * @Auther kongyin
+ * @Date 2020/10/21 14:42
+ **/
+public @interface EnableLogging {
+
+}
---
+++ inner-intergration/uaa-server-spring-boot-starter/src/main/java/com/ycz/platform/log/annotation/LogAnnotation.java
@@ -0,0 +1,11 @@
+package com.ycz.platform.log.annotation;
+
+/**
+ * @ClassName LogAnnotation
+ * @Description 日志
+ * @Auther kongyin
+ * @Date 2020/10/21 14:43
+ **/
+public @interface LogAnnotation {
+
+}
