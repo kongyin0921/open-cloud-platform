390ec883773028a9f8a0dd4a78423bd3984999b0
kongyin <1401618234@qq.com>
2021-07-15 21:56:45 +0800

    MybatisPlusAutoConfigure

---
+++ ocp-common/ocp-common-mybatis/src/main/java/com/ocp/common/mybatis/MybatisPlusAutoConfigure.java
@@ -0,0 +1,41 @@
+package com.ocp.common.mybatis;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.ocp.common.mybatis.resolver.SqlFilterArgumentResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * mybatis plus 统一配置
+ * @author kong
+ * @date 2021/07/15 20:27
+ * blog: http://blog.kongyin.ltd
+ */
+@Configuration(proxyBeanMethods=false)
+public class MybatisPlusAutoConfigure implements WebMvcConfigurer {
+    /**
+     * SQL 过滤器避免SQL 注入
+     * @param argumentResolvers
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SqlFilterArgumentResolver());
+    }
+
+    /**
+     * 分页插件, 对于单一数据库类型来说,都建议配置该值,避免每次分页都去抓取数据库类型
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+
+}
