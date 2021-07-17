9e83eca3d07a796a681210cb6931d789f2c3769a
kongyin <1401618234@qq.com>
2021-07-17 22:17:22 +0800

    zk

---
+++ ocp-common/ocp-common-core/src/main/java/com/ocp/common/constant/CommonConstant.java
@@ -0,0 +1,15 @@
+package com.ocp.common.constant;
+
+/**
+ * 全局公共常量
+ * @author kong
+ * @date 2021/07/17 21:55
+ * blog: http://blog.kongyin.ltd
+ */
+public interface CommonConstant {
+
+    /**
+     * 文件分隔符
+     */
+    String PATH_SPLIT = "/";
+}
--- ocp-common/ocp-common-zookeeper/src/main/java/com/ocp/common/zookeeper/ZookeeperAutoConfiguration.java
+++ ocp-common/ocp-common-zookeeper/src/main/java/com/ocp/common/zookeeper/ZookeeperAutoConfiguration.java
@@ -1,13 +1,33 @@
 package com.ocp.common.zookeeper;
 
 import com.ocp.common.zookeeper.properties.ZookeeperProperty;
+import com.ulisesbocchio.jasyptspringboot.annotation.ConditionalOnMissingBean;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
+ * zookeeper 配置类
+ *
  * @author kong
  * @date 2021/07/16 22:00
  * blog: http://blog.kongyin.ltd
  */
 @EnableConfigurationProperties(ZookeeperProperty.class)
+@ComponentScan
 public class ZookeeperAutoConfiguration {
+
+    @Bean(initMethod = "start",destroyMethod = "close")
+    @ConditionalOnMissingBean
+    public CuratorFramework curatorFramework(ZookeeperProperty property){
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(property.getBaseSleepTime(), property.getMaxRetries());
+        return CuratorFrameworkFactory.builder().connectString(property.getConnectString())
+                .connectionTimeoutMs(property.getConnectionTimeout())
+                .sessionTimeoutMs(property.getSessionTimeout())
+                .retryPolicy(retryPolicy).build();
+    }
 }
