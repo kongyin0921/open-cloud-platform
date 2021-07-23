63934c4df831e05270ef27cd83409b0409a4c5fe
kongyin <1401618234@qq.com>
2021-07-23 21:58:21 +0800

    feat:es

--- ocp-common/ocp-common-elasticsearch/src/main/java/com/ocp/common/es/properties/RestClientPoolProperties.java
+++ ocp-common/ocp-common-elasticsearch/src/main/java/com/ocp/common/es/properties/RestClientPoolProperties.java
@@ -1,6 +1,8 @@
 package com.ocp.common.es.properties;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * es的httpClient连接池配置
@@ -9,5 +11,27 @@ import lombok.Data;
  * blog: http://blog.kongyin.ltd
  */
 @Data
+@ConfigurationProperties(prefix = "ocp.elasticsearch.rest-pool")
+@RefreshScope
 public class RestClientPoolProperties {
+    /**
+     * 链接建立超时时间
+     */
+    private Integer connectTimeOut = 1000;
+    /**
+     * 等待数据超时时间
+     */
+    private Integer socketTimeOut = 30000;
+    /**
+     * 连接池获取连接的超时时间
+     */
+    private Integer connectionRequestTimeOut = 500;
+    /**
+     * 最大连接数
+     */
+    private Integer maxConnectNum = 30;
+    /**
+     * 最大路由连接数
+     */
+    private Integer maxConnectPerRoute = 10;
 }
